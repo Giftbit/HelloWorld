2a7957c45fec39016260b2269071d0070b9d9599
Peter Locke <peter@kiind.me>
2016-02-24 21:25:21 -0800

    Made main class a .java file to show mixing groovy and java

---
+++ src/main/groovy/com/giftbit/codingexercise/CodingExercise.java
@@ -0,0 +1,11 @@
+package com.giftbit.codingexercise;
+
+class CodingExercise {
+
+    //do not change
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.search("Hello World");
+        System.out.println( hello.whatShouldISay() );
+    }
+}
